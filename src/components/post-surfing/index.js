9ccc2e4c6f5539bfb2197786bc58a2ef62250b4c
The Anh <dotheanh12399@gmail.com>
2022-07-05 17:08:36 +0700

    React post

--- src/components/post-surfing/index.js
+++ src/components/post-surfing/index.js
@@ -1,6 +1,7 @@
 Component({
   data: {
     isSavedPost: false,
+    votedPost: 0,
   },
 
   props: {
@@ -19,6 +20,7 @@ Component({
       commentsCount: 0,
       viewsCount: 0,
       isSaved: false,
+      voted: 0, // -1 = downvoted, 1 = upvoted, 0 = not voted 
     },
   },
   
@@ -27,7 +29,8 @@ Component({
       console.log("Render post-surfing");
       console.log(this.props.post);
       this.setData({
-        isSavedPost: this.props.post.isSaved
+        isSavedPost: this.props.post.isSaved,
+        votedPost: this.props.post.voted,
       });
     },
   
@@ -35,10 +38,24 @@ Component({
     methods: {
       onClickBookmarkButton() {
         // TODO: TA call API to save post
-      console.log("// TODO: TA call API to save post");
+        console.log("// TODO: TA call API to save post");
         this.setData({
           isSavedPost: !this.data.isSavedPost
         });
+      },
+      onClickUpVote() {
+        // TODO: TA call API to UpVote
+        console.log("// TODO: TA call API to UpVote");
+        let newVotedPost;
+        if (this.data.votedPost === 0) newVotedPost = 1;
+        else if (this.data.votedPost === 1) newVotedPost = 0;
+        this.setData({
+          votedPost: newVotedPost
+        });
+      },
+      onClickComment() {
+        // TODO: TA call API to Comment
+        console.log("// TODO: TA call API to Comment");
       },
     }
   });
